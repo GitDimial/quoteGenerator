c7819295aeb2675ed704fdf26c4388d6e38db614
Dmytro Knyshuk <contact.dimial@gmail.com>
2023-02-02 14:51:35 +0200

    Last version. Add spinner

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+loader.hidden = true;
 let randomQuote = 0;
 //Get Quotes function here
 async function getQuote(){
+    showLoadingSpinner();
     const quoteUrl = 'https://gitdimial.github.io/quoteGenerator/data/quotes.json';
     randomQuote = Math.floor(Math.random() * 8000);
     try{
@@ -26,6 +29,7 @@ async function getQuote(){
         }else{
             authorText.innerText = data[randomQuote].author;
         }
+       setTimeout(removeLoadingSpinner, 500);
     }catch(err){
         console.log('Nice, you have an error: ' + err);
     }
@@ -39,6 +43,19 @@ function tweetQuote(){
     window.open(twitterUrl, '_blank');
 }
 
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;    
+}
+
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 //Event listeners
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click',tweetQuote);
+
